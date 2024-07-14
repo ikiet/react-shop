6ed993df010ee76a083fb83ac27ec2f00c98dcd2
Kiet Cao <kietcao@tuta.io>
2024-07-14 19:54:14 +0900

    convert to use useReducer api

--- src/store/shopping-cart-context.jsx
+++ src/store/shopping-cart-context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
 import { DUMMY_PRODUCTS } from "../dummy-products.js";
 
 export const CartContext = createContext({
@@ -7,71 +7,85 @@ export const CartContext = createContext({
   onUpdateCartItemQuantity: () => {},
 });
 
+const shoppingCartReducer = (state, action) => {
+  const { type, payload } = action;
+  if (type === "ADD_ITEM") {
+    const { id } = payload;
+    const updatedItems = [...state.items];
+
+    const existingCartItemIndex = updatedItems.findIndex(
+      (cartItem) => cartItem.id === id,
+    );
+    const existingCartItem = updatedItems[existingCartItemIndex];
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+      updatedItems.push({
+        id: id,
+        name: product.title,
+        price: product.price,
+        quantity: 1,
+      });
+    }
+
+    return {
+      items: updatedItems,
+    };
+  } else if (type === "UPDATE_ITEM_QUANTITY") {
+    const { productId, amount } = payload;
+    const updatedItems = [...state.items];
+    const updatedItemIndex = updatedItems.findIndex(
+      (item) => item.id === productId,
+    );
+
+    const updatedItem = {
+      ...updatedItems[updatedItemIndex],
+    };
+
+    updatedItem.quantity += amount;
+
+    if (updatedItem.quantity <= 0) {
+      updatedItems.splice(updatedItemIndex, 1);
+    } else {
+      updatedItems[updatedItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+    };
+  } else {
+    return state;
+  }
+};
+
 export default ({ children }) => {
-  const [shoppingCart, setShoppingCart] = useState({
+  const [shoppingCart, shoppingCartDispatch] = useReducer(shoppingCartReducer, {
     items: [],
   });
 
-  function handleAddItemToCart(id) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-
-      const existingCartItemIndex = updatedItems.findIndex(
-        (cartItem) => cartItem.id === id,
-      );
-      const existingCartItem = updatedItems[existingCartItemIndex];
-
-      if (existingCartItem) {
-        const updatedItem = {
-          ...existingCartItem,
-          quantity: existingCartItem.quantity + 1,
-        };
-        updatedItems[existingCartItemIndex] = updatedItem;
-      } else {
-        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
-        updatedItems.push({
-          id: id,
-          name: product.title,
-          price: product.price,
-          quantity: 1,
-        });
-      }
-
-      return {
-        items: updatedItems,
-      };
-    });
-  }
-
-  function handleUpdateCartItemQuantity(productId, amount) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-      const updatedItemIndex = updatedItems.findIndex(
-        (item) => item.id === productId,
-      );
-
-      const updatedItem = {
-        ...updatedItems[updatedItemIndex],
-      };
-
-      updatedItem.quantity += amount;
-
-      if (updatedItem.quantity <= 0) {
-        updatedItems.splice(updatedItemIndex, 1);
-      } else {
-        updatedItems[updatedItemIndex] = updatedItem;
-      }
-
-      return {
-        items: updatedItems,
-      };
-    });
-  }
-
   const cartValue = {
     items: shoppingCart.items,
-    onAddItemToCart: handleAddItemToCart,
-    onUpdateCartItemQuantity: handleUpdateCartItemQuantity,
+    onAddItemToCart: (id) =>
+      shoppingCartDispatch({
+        type: "ADD_ITEM",
+        payload: {
+          id,
+        },
+      }),
+    onUpdateCartItemQuantity: (productId, amount) =>
+      shoppingCartDispatch({
+        type: "UPDATE_ITEM_QUANTITY",
+        payload: {
+          productId,
+          amount,
+        },
+      }),
   };
 
   return (
